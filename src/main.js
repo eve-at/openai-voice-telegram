8e13a4b5025aaeedb0326cfd485768dc0438ddb4
Alexandre Muraviov <amuraviov@globalia.ca>
2023-08-27 13:13:57 -0400

    download ogg file from telegram

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 import { Telegraf } from "telegraf"
 import { message } from "telegraf/filters"
 import config from "config"
+import { ogg } from "./ogg.js"
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
 // text message
 bot.on(message('text'), async ctx => {
-    await ctx.reply(JSON.stringify(ctx.message.text, null, 2))
+    try {
+        await ctx.reply(JSON.stringify(ctx.message.text, null, 2))
+    } catch (e) {
+        console.log('Voice message error ', e.message);
+    }
 })
 
 // voice message
 bot.on(message('voice'), async ctx => {
-    await ctx.reply(JSON.stringify(ctx.message.voice, null, 2))
+    try {
+        //await ctx.reply(JSON.stringify(ctx.message, null, 2))
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+        const userId = String(ctx.message.from.id)
+        await ctx.reply(JSON.stringify(link, null, 2))
+        console.log('Voice url: ', link.href);
+
+        const offPath = await ogg.create(link.href, userId)
+    } catch (e) {
+        console.log('Voice message error ', e.message);
+    }
 })
 
 bot.command('start', async (ctx) => {
---
+++ src/ogg.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { createWriteStream } from "fs"
+import { dirname, resolve} from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+class OggConverter {
+    constructor() {
+
+    }
+
+    toMp3() {
+
+    }
+
+    async create(url, filename) {
+        try {
+            const oggPath = resolve(__dirname, '../voices', `${filename}.ogg`)
+            const response = await axios({
+                method: 'get',
+                url,
+                responseType: 'stream'
+            })
+            return new Promise((resolve) => {
+                const stream = createWriteStream(oggPath)
+                response.data.pipe(stream)
+                stream.on('finish', () => resolve(oggPath))
+            })
+        } catch (e) {
+            console.log('Axios error ', e.message)
+        }
+
+    }
+}
+
+export const ogg = new OggConverter()
